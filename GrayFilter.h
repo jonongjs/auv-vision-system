161632fff50cff608cf4f3f47529e2a9a9660521
Jon Ong <jonongjs@gmail.com>
2013-03-25 22:33:19 +0800

    Added gray and HSV filters.

---
+++ GrayFilter.h
@@ -0,0 +1,25 @@
+// GrayFilter.h
+//
+// Converts an image to grayscale
+
+#ifndef GRAYFILTER_H
+#define GRAYFILTER_H
+
+#include "ImageFilterBase.h"
+
+class GrayFilter : public ImageFilterBase
+{
+    Q_OBJECT
+
+public:
+    void setProperty(const std::string& /*propertyName*/, const std::string& /*value*/) { }
+    std::string getProperty(const std::string& /*propertyName*/) { return ""; }
+
+public slots:
+    void setImage(const cv::Mat& image);
+
+private:
+    cv::Mat output;
+};
+
+#endif//GRAYFILTER_H
